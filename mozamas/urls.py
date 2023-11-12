c14ecaa2ad2da1766b66bd46173cc19b6a0e66e1
princekay360 <ddennispk@gmail.com>
2023-11-12 23:25:23 +0000

    ~

--- mozamas/urls.py
+++ mozamas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('mozamas', views.index, name='mozamas_index'),
-    path('mozamas/login', views.login, name='mozamas_login'),
+    path('mozamas/login', views.login, name='m_login'),
 ]
